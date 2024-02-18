bf82d45730a9164f7580de71c1e887fab4fec184
Runa Brandes <runabrandes@gmail.com>
2024-02-18 17:47:31 +0000

    Added game rules and modified name input

--- run.py
+++ run.py
@@ -1,12 +1,28 @@
 """
 Greeting message to user 
 """
+print("_________________________________\n")
 print("Welcome to this Tic Tac Toe game!")
+print("_________________________________\n")
+
+def username_input():
+    username = input("Please enter your name: \n")
+    if username == "":
+        print ("Input invalid.... Please enter a username!")
+    elif not username.isalpha():
+        print("The username cannot contain numbers, please use letters only!")
+    else:
+        #break
+        return username
 
 """
 Outlining the rules to the user
 """
-#print("")
+print("This Tic Tac Toe game is played by putting down X's and O's.\n")
+print("Users take turns putting down their symbol in one of the board fields (1-9)\n")
+print("The first user to have three X's or O's in a row wins!\n")
+print("This can be either horizontal, vertical or diagonal.\n")
+print("LET'S BEGIN!\n\n")
 
 """
 Creating a tic tac toe board.
@@ -23,3 +39,4 @@ boardPositions = {
     '8': '8',
     '9': '9',
 }
+
