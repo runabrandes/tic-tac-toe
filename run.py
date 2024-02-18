f96485a6a71581cd48e986b7b74c29a1b1f3bb62
Runa Brandes <runabrandes@gmail.com>
2024-02-18 14:33:55 +0000

    Added dictionary for game board and greeting message for user

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Greeting message to user 
+"""
+print("Welcome to this Tic Tac Toe game!")
+
+"""
+Outlining the rules to the user
+"""
+#print("")
+
+"""
+Creating a tic tac toe board.
+"""
+
+boardPositions = {
+    '1': '1',
+    '2': '2',
+    '3': '3',
+    '4': '4',
+    '5': '5',
+    '6': '6',
+    '7': '7',
+    '8': '8',
+    '9': '9',
+}
