e2b76f80b0ff4b5c6c9045e0a0e37c0a2ae21858
Runa Brandes <runabrandes@gmail.com>
2024-02-19 20:31:13 +0000

    Updated game rules and started implementing function to place X's on game board

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import random
+
 """
 Greeting message to user 
 """
@@ -18,26 +20,29 @@ def username_input():
 """
 Outlining the rules to the user
 """
-print("This Tic Tac Toe game is played by putting down X's and O's.\n")
-print("Users take turns putting down their symbol in one of the board fields (1-9)\n")
-print("The first user to have three X's or O's in a row wins!\n")
-print("This can be either horizontal, vertical or diagonal.\n")
-print("LET'S BEGIN!\n\n")
+def gameRules():
+    print("This Tic Tac Toe game is played by putting down X's and O's.\n")
+    print("Users take turns putting down their symbol in one of the board fields (1-9)\n")
+    print("The first user to have three X's or O's in a row wins!\n")
+    print("This can be either horizontal, vertical or diagonal.\n")
+    print("**The layout of the board is 1,2,3 in the top row,\n")
+    print("4,5,6 in the middle and 7,8,9 in the bottom row.**\n")
+    print("LET'S BEGIN!\n\n")
 
 """
 Creating a tic tac toe board.
 """
 
 boardPositions = {
-    '1': '1',
-    '2': '2',
-    '3': '3',
-    '4': '4',
-    '5': '5',
-    '6': '6',
-    '7': '7',
-    '8': '8',
-    '9': '9',
+    '1': '-',
+    '2': '-',
+    '3': '-',
+    '4': '-',
+    '5': '-',
+    '6': '-',
+    '7': '-',
+    '8': '-',
+    '9': '-',
 }
 
 """
@@ -46,24 +51,30 @@ Using dictionary keys and values from "boardPositions".
 """
 
 def printGameBoard(board):
-    print("--+---+--")
     print(board['1'] + ' | ' + board['2'] + ' | ' + board['3'])
     print("--+---+--")
     print(board['4'] + ' | ' + board['5'] + ' | ' + board['6'])
     print("--+---+--")
     print(board['7'] + ' | ' + board['8'] + ' | ' + board['9'])
-    print("--+---+--")
 
-
-player1 == "X"
-player2 == "O"
-
-#def userMoveInput():
-    
-
-
-#def winningCombinations():
 #012,036,048,147,246,345,258,678
+def winningCombinations(board, recentPlayer):
 
+
+checkForWin = False
+
+gameRules()
 username_input()
+
+player = "X"
+
+while checkForWin == False:
+    userSelection = input(f'Your turn {username_input}! Please enter a board position:')
+    boardPositions[userSelection] = player
+    printGameBoard(boardPositions)
+
+#def playerInput():
+
+
+
 printGameBoard(boardPositions)
